b406cc313ae713b284fe8bdd3530bde16d5d1124
Andrew <chen.andrew772@gmail.com>
2021-04-21 14:24:04 -0700

    Add - for loop to iterate and modify src Add - onclick event Add - array item to new array

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,12 @@
+let images = document.querySelectorAll(".shop-item-image");
+let cart_items = document.querySelectorAll("cart-row");
+let num = 0
+
+for (image of images) {
+    num += 1;
+    image.src = `Images/Album${num}.png`;
+}
+
 const albums = [
     { name: "Album 1", price: 12.99, inCart: false }, 
     { name: "Album 2", price: 14.99, inCart: false }, 
@@ -5,5 +14,20 @@ const albums = [
     { name: "Album 4", price: 19.99, inCart: false }
   ];
 
-  
-albums
+let cart_status = []
+images.forEach(item => {
+    item.addEventListener('click', (ev) => {
+        let selected = ev.currentTarget
+        console.log(selected)
+        let cart_selected = selected.getAttribute('id');
+        console.log(cart_selected)
+        for (album of albums) {
+            let { name, price, inCart } = album;
+            album.inCart = true
+            if (cart_selected === name){
+                cart_status.push(album)
+                console.log(cart_status)
+            }
+        }
+    })
+});
